a69d0e48a67aa1ee07cc9e0b8d63295673ad4323
Christopher Najewicz <christopher.najewicz@parsleyhealth.com>
2022-11-30 13:25:00 -0500

    fix: use local file for transform/parse

--- bit-tag-export/docker/entrypoint.sh
+++ bit-tag-export/docker/entrypoint.sh
@@ -3,15 +3,15 @@
 set -euo pipefail
 
 BIT_TOKEN=${1:?"no BIT_TOKEN provided"}
+BIT_CHANGES_OUTPUT_FILE=${GITHUB_WORKSPACE}/bit-tag.out
 
 bit config set analytics_reporting false
 bit config set anonymous_reporting false
 bit config set user.token ${BIT_TOKEN}
 bit install
 
-BIT_CHANGES_RAW_OUTPUT=$(bit tag --persist)
+bit tag --persist >${BIT_CHANGES_OUTPUT_FILE}
 
-echo "BIT_CHANGES_RAW_OUTPUT=${BIT_CHANGES_RAW_OUTPUT}" >>$GITHUB_ENV
-echo "BIT_CHANGES=$(echo ${BIT_CHANGES_RAW_OUTPUT} | grep '     > ' | sed 's/     > />/g' | tr '\n' ',' | sed 's/,/\\n/g')" >>$GITHUB_ENV
+echo "BIT_CHANGES=\"$(cat ${BIT_CHANGES_OUTPUT_FILE} | grep '     > ' | sed 's/     > />/g' | tr '\n' ',' | sed 's/,/\\n/g')\"" >>${GITHUB_ENV}
 
 bit export
