ad34374f962722fb2021bc487b487c48b2a53340
Ibrahim Haruna <Ibrahim.haruna@parsleyhealth.com>
2023-07-26 16:56:00 -0600

    modified Dockerfile, action.yml, and entrypoint.sh for the Bit Tag Export

--- bit-tag-export/docker/entrypoint.sh
+++ bit-tag-export/docker/entrypoint.sh
@@ -2,7 +2,12 @@
 
 set -euo pipefail
 
-BIT_TOKEN=${1:?"no BIT_TOKEN provided"}
+BIT_TOKEN=${1:-$BIT_TOKEN_ENV}
+
+if [ -z "$BIT_TOKEN" ]; then
+  echo "Error: No BIT_TOKEN provided. Please provide the Bit token as an input to this action or set the BIT_TOKEN_ENV environment variable."
+  exit 1
+fi
 BIT_CHANGES_OUTPUT_FILE=${GITHUB_WORKSPACE}/bit-tag.out
 
 bit config set analytics_reporting false
